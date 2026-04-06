8637ee5c920ce1b8e3c6f4e6ddbbe5c5e91ed845
Rick Pastoor <rickpastoor@gmail.com>
2026-04-06 22:13:36 +0200

    Fix version for real 2

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup'
+import pkg from './package.json'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm'],
+  target: 'node20',
+  clean: true,
+  define: {
+    __CLI_VERSION__: JSON.stringify(pkg.version),
+  },
+})
